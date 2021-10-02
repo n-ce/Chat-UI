082b52b57575c4668c82fc942682b01c6020f534
n-ce <abrocks1120@gmail.com>
2021-10-02 12:05:12 +0530

    local storage theme, code formatted

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
 var theme = document.getElementsByClassName('theme');
 
 
-function themeChange(theme,theme2) {
-  document.querySelector(':root').style.setProperty('--primary', theme);
-  document.querySelector(':root').style.setProperty('--primary2', theme2);
+if (localStorage.getItem('scheme')) {
+  themeChange(localStorage.getItem('scheme'), localStorage.getItem('scheme2'));
+}
 
+
+function themeChange(theme, theme2) {
+  document.querySelector(':root').style.setProperty('--primary', theme);
+  document.querySelector(':root').style.setProperty('--secondary', theme2);
+  localStorage.setItem('scheme', theme);
+  localStorage.setItem('scheme2', theme2);
 }
 
 
 theme[0].onclick = function() {
-  themeChange('orange','orangered');
+  themeChange('orange', 'darkorange');
 }
 theme[1].onclick = function() {
-  themeChange('lightblue','cyan');
+  themeChange('lightskyblue', 'deepskyblue');
 }
 theme[2].onclick = function() {
-  themeChange('lightseagreen','seagreen');
+  themeChange('mediumseagreen', 'seagreen');
+}
+theme[3].onclick = function() {
+  themeChange('mediumpurple', 'rebeccapurple');
+}
+theme[4].onclick = function() {
+  themeChange('dimgray', 'black');
 }
 
-document.getElementById('send').onclick=function(){
+document.getElementById('send').onclick = function() {
   var list = document.createElement('li');
-  list.innerText=document.querySelector('input').value;
-  list.className="sender";
+  list.innerText = document.querySelector('input').value;
+  list.className = "sender";
   document.getElementById('messages').appendChild(list);
 }
