f8f77fdd3768bdfd3403d2e1394cdc1689a8267a
n-ce <abrocks1120@gmail.com>
2021-10-02 11:25:48 +0530

    Secondary Build Finish

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 var theme = document.getElementsByClassName('theme');
 
 
-function themeChange(theme) {
+function themeChange(theme,theme2) {
   document.querySelector(':root').style.setProperty('--primary', theme);
+  document.querySelector(':root').style.setProperty('--primary2', theme2);
 
 }
 
 
 theme[0].onclick = function() {
-  themeChange('orange');
+  themeChange('orange','orangered');
 }
 theme[1].onclick = function() {
-  themeChange('lightblue');
+  themeChange('lightblue','cyan');
 }
 theme[2].onclick = function() {
-  themeChange('seagreen');
+  themeChange('lightseagreen','seagreen');
+}
+
+document.getElementById('send').onclick=function(){
+  var list = document.createElement('li');
+  list.innerText=document.querySelector('input').value;
+  list.className="sender";
+  document.getElementById('messages').appendChild(list);
 }
